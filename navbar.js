0a5019634f9aa8e8530bc55c9706db2f62981c82
mohit0519 <mohitbchettri@gmail.com>
2026-02-03 16:51:46 +0545

    rearranged and structured the files

--- navbar.js
+++
@@ -1,4 +0,0 @@
-// fetch("../navbar.html")
-//   .then(res => res.text())
-//   .then(html => document.getElementById("navbar").innerHTML = html)
-//   .catch(err => console.log("Navbar load error:", err));
